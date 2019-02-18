59b5cd031aa7a6694fcf3255224f5b846fe29026
Meghan13 <meghangraham13@gmail.com>
2019-02-18 13:44:22 -0800

    Snake Update

--- src/main/java/io/battlesnake/starter/Snake.java
+++ src/main/java/io/battlesnake/starter/Snake.java
@@ -122,7 +122,7 @@ public class Snake {
         public Map<String, String> move(JsonNode moveRequest) {
             Map<String, String> response = new HashMap<>();
             //JsonNode req = JSON_MAPPER.readTree(moveRequest.body());
-            int health = moveRequest.get("health").asInt();
+            int health = moveRequest.get("you").get("health").asInt();
             System.out.println("Health is: " + health);
             response.put("move", "right");
             return response;
